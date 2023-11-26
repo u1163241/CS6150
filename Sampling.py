92e61f3e1b82444148466b5961732dbb0a5353ef
Di Wu <u1163241@utah.edu>
2023-11-25 19:04:54 -0700

    Finish sampling

---
+++ Sampling.py
@@ -0,0 +1,18 @@
+import random
+
+sList = [20, 100, 400]
+answer = []
+for i in sList:
+    temp = []
+    for r in range(100):
+        count = 0
+        for t in range(i):
+            rand = random.randint(0, 1000000)
+            if rand <= 520000:
+                count += 1
+        if count > (i / 2):
+            temp.append(1)
+        else:
+            temp.append(0)
+    answer.append(sum(temp) / len(temp))
+print(answer)
